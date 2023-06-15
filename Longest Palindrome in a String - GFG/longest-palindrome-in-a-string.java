941779643b8d8de60e70cacb191d327b65d05002
Shivam Tiwari <113517640+shivammiet@users.noreply.github.com>
2023-06-15 22:50:03 +0530

    Added solution - LeetHub

---
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.longestPalin(S));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String solve(String S,int l,int h){
+        while(l>=0 && h<S.length() && S.charAt(l)==S.charAt(h)){
+            l--;
+            h++;
+        }
+        return S.substring(l+1,h);
+    }
+    static String longestPalin(String S){
+        // code here
+        String ans="";
+        if(S.length()<2)
+        return S;
+        for(int i=0;i<S.length()-1;i++){
+            String even=solve(S,i,i);
+            if(even.length()>ans.length()){
+                ans=even;
+            }
+            String odd=solve(S,i,i+1);
+            if(odd.length()>ans.length()){
+                ans=odd;
+            }
+        }
+        return ans;
+    }
+}
