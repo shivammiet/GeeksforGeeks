ec3ca88788a1028342b365d4e8ea2e6372146183
Shivam Tiwari <113517640+shivammiet@users.noreply.github.com>
2023-10-10 16:01:37 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.java
+++ Peak element - GFG/peak-element.java
@@ -51,20 +51,22 @@ class Solution
     // n: size of array a[]
     public int peakElement(int[] arr, int n)
     {
-        return peakElementUtil(arr, 0, n - 1, n);
-    }
+        int low = 0, high = n-1;
 
-    private int peakElementUtil(int[] arr, int low, int high, int n) {
-        int mid = low + (high - low) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-        if ((mid == 0 || arr[mid - 1] <= arr[mid]) && (mid == n - 1 || arr[mid + 1] <= arr[mid])) {
-            return mid;
+            if ((mid == 0 || arr[mid-1] <= arr[mid]) && (mid == n-1 || arr[mid+1] <= arr[mid])) {
+                return mid;
+            }
+            else if (mid > 0 && arr[mid-1] > arr[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
         }
 
-        if (mid > 0 && arr[mid - 1] > arr[mid]) {
-            return peakElementUtil(arr, low, mid - 1, n);
-        } else {
-            return peakElementUtil(arr, mid + 1, high, n);
-        }
+        return -1; // No peak element found
     }
 }
